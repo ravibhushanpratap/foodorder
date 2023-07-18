cfa0959b7d37d2cb7af94d9f9914150ebe8301fa
ravibhushanpratap <139762127+ravibhushanpratap@users.noreply.github.com>
2023-07-17 19:14:09 -1200

    Update login.js

--- javascript/login.js
+++ javascript/login.js
@@ -5,7 +5,7 @@ function validate(){
 	var username = document.getElementById("username").value;
 	var password = document.getElementById("password").value;
 
-	if ( username == "informatics" && password == "informatics123"){
+	if ( username == "foodie" && password == "foodie123"){
 		alert ("Login successfully");
 		window.location = "success.html"; //redirecting to other page
 		return false;
